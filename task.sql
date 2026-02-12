7c5e6d6354718d248d9a7277ff088257aa995413
dima.yeromin <dima.yeromin@gmail.com>
2026-02-12 21:40:35 +0100

    done

--- task.sql
+++ task.sql
@@ -9,9 +9,32 @@ CREATE TABLE Countries (
 ) ENGINE=InnoDB;
 
 -- Create a table for caching GeoIP data (Columns: ID, IP Range, CountryID)
+CREATE TABLE GeoIPCache (
+    ID INT NOT NULL,
+    IPRange VARCHAR(255) NOT NULL,
+    CountryID INT NOT NULL,
+    PRIMARY KEY (ID)
+) ENGINE=MEMORY;
 
 -- Create a table for storing product descriptions for different countries (Columns: ID, CountryID, ProductID, Description )
+CREATE TABLE ProductDescription (
+    ID INT NOT NULL,
+    CountryID INT NOT NULL,
+    ProductID INT NOT NULL,
+    Description TEXT NOT NULL,
+    PRIMARY KEY (ID)
+) ENGINE=MyISAM;
 
 -- Create a table for storing logs. For now we don't need to save them, but we need to implement functionality (Columns: ID, Time, LogRecord)
+CREATE TABLE Logs (
+    ID INT NOT NULL,
+    Timestamp DATETIME NOT NULL,
+    Message TEXT NOT NULL
+) ENGINE=BLACKHOLE;
 
 -- Create a table for storing reporting data, which will be send to a separate application in the CSV format for analytics purposes (Columns:  Date, ProductName, Orders)
+CREATE TABLE ProductReporting (
+    Date DATE NOT NULL,
+    ProductName VARCHAR(255) NOT NULL,
+    Orders INT NOT NULL
+) ENGINE=CSV;
